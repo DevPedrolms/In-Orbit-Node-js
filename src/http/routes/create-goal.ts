d3f8991175fce9d0013e9c95cfe772c8f458385d
DevPedrolms <devpedrolms@hotmail.com>
2024-11-19 16:41:14 -0300

    Authenticated routes in fastify

--- src/http/routes/create-goal.ts
+++ src/http/routes/create-goal.ts
@@ -1,17 +1,20 @@
 import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { createGoal } from '../../functions/create-goal'
+import { authenticateUserHook } from '../hooks/authenticate-user'
 
 export const createGoalRoute: FastifyPluginAsyncZod = async app => {
   app.post(
     '/goals',
     {
+      onRequest: [authenticateUserHook],
       schema: {
         tags: ['goals'],
         description: 'create a goal',
         body: z.object({
           title: z.string(),
           desiredWeeklyFrequency: z.number().int().min(1).max(7),
+          userId: z.string(),
         }),
         reponse: {
           201: z.null(),
@@ -19,11 +22,12 @@ export const createGoalRoute: FastifyPluginAsyncZod = async app => {
       },
     },
     async (request, reply) => {
-      const { title, desiredWeeklyFrequency } = request.body
+      const { title, desiredWeeklyFrequency, userId } = request.body
 
       await createGoal({
         title,
         desiredWeeklyFrequency,
+        userId,
       })
       return reply.status(201).send()
     }
