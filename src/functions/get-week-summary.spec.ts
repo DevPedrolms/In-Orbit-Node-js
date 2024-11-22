a2a6251ed867cb2e83daeee16f63a384560f3fb7
DevPedrolms <devpedrolms@hotmail.com>
2024-11-22 17:10:43 -0300

    Navigation by weeks

--- src/functions/get-week-summary.spec.ts
+++ src/functions/get-week-summary.spec.ts
@@ -4,10 +4,15 @@ import { makeGoal } from '../../tests/factories/make-goal'
 import { makeGoalCompletion } from '../../tests/factories/make-goal-completion'
 import { getWeekPendingGoals } from './get-week-pending-goals'
 import { getWeekSummary } from './get-week-summary'
+import dayjs from 'dayjs'
 
 describe('get week summary', () => {
   it('should be able a get a week summary', async () => {
     const user = await makeUser()
+
+    const weekStartsAt = dayjs(new Date(2024, 10, 22))
+      .startOf('week')
+      .toDate()
 
     const goal1 = await makeGoal({
       userId: user.id,
@@ -25,37 +30,69 @@ describe('get week summary', () => {
       desiredWeeklyFrequency: 4,
     })
 
-    await makeGoalCompletion({ goalId: goal1.id })
-    await makeGoalCompletion({ goalId: goal1.id })
-    await makeGoalCompletion({ goalId: goal2.id })
-    await makeGoalCompletion({ goalId: goal3.id })
-    await makeGoalCompletion({ goalId: goal3.id })
-    await makeGoalCompletion({ goalId: goal3.id })
+    await makeGoalCompletion({
+      goalId: goal1.id,
+      createdAt: dayjs(weekStartsAt).add(2, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal1.id,
+      createdAt: dayjs(weekStartsAt).add(3, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal2.id,
+      createdAt: dayjs(weekStartsAt).add(4, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal3.id,
+      createdAt: dayjs(weekStartsAt).add(5, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal3.id,
+      createdAt: dayjs(weekStartsAt).add(5, 'days').toDate(),
+    })
+    await makeGoalCompletion({
+      goalId: goal3.id,
+      createdAt: dayjs(weekStartsAt).add(3, 'days').toDate(),
+    })
 
     const result = await getWeekSummary({
       userId: user.id,
+      weekStartsAt,
     })
 
-    console.log(result)
-
-    // expect(result).toEqual({
-    //   pendingGoals: expect.arrayContaining([
-    //     expect.objectContaining({
-    //       title: 'meditar',
-    //       desiredWeeklyFrequency: 3,
-    //       completionCount: 2,
-    //     }),
-    //     expect.objectContaining({
-    //       title: 'correr',
-    //       desiredWeeklyFrequency: 2,
-    //       completionCount: 1,
-    //     }),
-    //     expect.objectContaining({
-    //       title: 'Ler docs',
-    //       desiredWeeklyFrequency: 4,
-    //       completionCount: 3,
-    //     }),
-    //   ]),
-    // })
+    expect(result).toEqual({
+      summary: {
+        completed: 6,
+        total: 9,
+        goalsPerDay: {
+          '2024-11-22': expect.arrayContaining([
+            expect.objectContaining({
+              title: 'Ler docs',
+            }),
+            expect.objectContaining({
+              title: 'Ler docs',
+            }),
+          ]),
+          '2024-11-21': expect.arrayContaining([
+            expect.objectContaining({
+              title: 'correr',
+            }),
+          ]),
+          '2024-11-20': expect.arrayContaining([
+            expect.objectContaining({
+              title: 'meditar',
+            }),
+            expect.objectContaining({
+              title: 'Ler docs',
+            }),
+          ]),
+          '2024-11-19': expect.arrayContaining([
+            expect.objectContaining({
+              title: 'meditar',
+            }),
+          ]),
+        },
+      },
+    })
   })
 })
--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -5,11 +5,15 @@ import dayjs from 'dayjs'
 
 interface GetWeekSummaryResponse {
   userId: string
+  weekStartsAt: Date
 }
 
-export async function getWeekSummary({ userId }: GetWeekSummaryResponse) {
-  const firstDayOfWeek = dayjs().startOf('week').toDate()
-  const lastDayOfWeek = dayjs().endOf('week').toDate()
+export async function getWeekSummary({
+  userId,
+  weekStartsAt,
+}: GetWeekSummaryResponse) {
+  const firstDayOfWeek = weekStartsAt
+  const lastDayOfWeek = dayjs(weekStartsAt).endOf('week').toDate()
 
   const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
     db
