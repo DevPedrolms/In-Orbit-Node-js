e1cdcfb98cca931e16ebe13584dd3904e0754511
DevPedrolms <devpedrolms@hotmail.com>
2024-12-08 17:21:46 -0300

    Calculating level user level

---
+++ src/modules/gamification.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from 'vitest'
+import {
+  calculateExperienceToLevel,
+  calculateLevelFromExperience,
+} from './gamification'
+
+test(' experiencte to level', () => {
+  const exp1 = calculateExperienceToLevel(1)
+  const exp2 = calculateExperienceToLevel(2)
+  const exp4 = calculateExperienceToLevel(4)
+
+  expect(exp1).toEqual(0)
+  expect(exp2).toEqual(26)
+  expect(exp4).toEqual(43)
+})
+
+test(' level to experience', () => {
+  const level1 = calculateLevelFromExperience(15)
+  const level2 = calculateLevelFromExperience(27)
+  const level4 = calculateLevelFromExperience(26 + 33 + 43)
+
+  expect(level1).toEqual(1)
+  expect(level2).toEqual(2)
+  expect(level4).toEqual(4)
+})
